73ac96eaf6712d3db38d3138503d5239b920f3a3
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-27 18:07:45 +0100

    Created index for types table

---
+++ resources/views/admin/types/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between my-5">
+                <h2>Tipi di progetti</h2>
+            </div>
+            <div class="col-12">
+                <div class="d-flex justify-content-center">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Slug</th>
+                                <th>Projects Count</th>
+                                <th>More</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($types as $type)
+                                <tr>
+                                    <td>{{ $type->id }}</td>
+                                    <td>{{ $type->name }}</td>
+                                    <td>{{ $type->slug }}</td>
+                                    <td>{{ count($type->projects) }}</td>
+                                    <td><a href="{{ route('admin.types.show', ['type' => $type->id]) }}">more</a></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,12 @@
     <div class="container">
         <div class="row">
             <div class="col-12 mt-5">
-                <a href="{{ route('admin.projects.index') }}">Vai ai miei progetti</a>
+                <div>
+                    <a href="{{ route('admin.projects.index') }}">Vai ai miei progetti</a>
+                </div>
+                <div>
+                    <a href="{{ route('admin.types.index') }}">Vai ai tipi di progetti</a>
+                </div>
             </div>
         </div>
     </div>
