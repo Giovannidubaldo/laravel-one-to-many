9f44ea1b5a031853c9b607b1b21eb1432206f1dd
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-27 17:06:20 +0100

    Added method on Project Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,5 +9,10 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'start_date', 'end_date', 'slug', 'image', 'description'];
+    protected $fillable = ['name', 'start_date', 'end_date', 'slug', 'image', 'description', 'type_id'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
